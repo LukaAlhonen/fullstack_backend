55ce6d755e0cc2da37d71fbe188569b4bc23c4ee
LukaA <luka.alhonen@protonmail.com>
2022-04-05 14:14:13 +0300

    Added mongodb functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
 
 const requestLogger = (req, res, next) => {
@@ -47,24 +50,20 @@ let notes = [
   }
 ]
 
-
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello, World!</h1>')
 })
 
 app.get('/api/notes', (req,res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if(note){
+  Note.findById(req.params.id).then(note => {
     res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -87,18 +86,18 @@ app.post('/api/notes', (req, res) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateID()
-  }
+  })
 
-  notes = notes.concat(note)
-  res.json(note)
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
 })
 
-const PORT=process.env.PORT || 3001
+const PORT=process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
