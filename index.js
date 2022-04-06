c47bbdb1779b8bbb1fc2d49e63352947c5dae114
LukaA <luka.alhonen@protonmail.com>
2022-04-06 18:16:36 +0300

    commit

--- index.js
+++ index.js
@@ -12,14 +12,19 @@ const requestLogger = (req, res, next) => {
   next()
 }
 
-const unknownEnpoint = (req, res) => {
-  res.status(404).send({
-    error: 'unknown endpoint'
-  })
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id'} )
+  }
+
+  next(error)
 }
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(errorHandler)
 app.use(cors())
 app.use(express.static('build'))
 
@@ -60,10 +65,16 @@ app.get('/api/notes', (req,res) => {
   })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note)
-  })
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if(note){
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res) => {
