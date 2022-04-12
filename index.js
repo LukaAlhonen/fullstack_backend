6790f6afdd5ce89ff4dc2cdebb9975ee81cd9584
Luka Alhonen <luka.alhonen@protonmail.com>
2022-04-12 10:59:30 +0300

    Commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const Note = require('./models/note')
 
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
 const requestLogger = (req, res, next) => {
   console.log('Method', req.method)
   console.log('Path', req.path)
@@ -12,48 +16,7 @@ const requestLogger = (req, res, next) => {
   next()
 }
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-
-  if(error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id'} )
-  }
-
-  next(error)
-}
-
-app.use(express.json())
 app.use(requestLogger)
-app.use(errorHandler)
-app.use(cors())
-app.use(express.static('build'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  },
-  {
-    id: 4,
-    content: "Big hairy balls",
-    date: new Date(),
-    important: true
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello, World!</h1>')
@@ -77,16 +40,13 @@ app.get('/api/notes/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateID = () => {
-  const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxID + 1
-}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
@@ -107,6 +67,39 @@ app.post('/api/notes', (req, res) => {
     res.json(savedNote)
   })
 })
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+  
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEnpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown enpoint' })
+}
+
+app.use(unknownEnpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id'} )
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT=process.env.PORT
 app.listen(PORT, () => {
